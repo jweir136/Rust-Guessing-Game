9e8068a9a255b09ab87ba81e537019bdd10f996b
jweir136 <jweir136@yahoo.com>
2022-02-17 08:29:55 -0500

    add game mechanics to codbase

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,39 @@
 
 mod slot_machine_utils;
 
+fn greet() {
+    println!("Welcome to Slot Machine!");
+}
+
+fn play(mut coins: usize) {
+    loop {
+        let mut line = String::new();
+        println!("Enter 1 to play, 0 to exit");
+        std::io::stdin().read_line(&mut line).unwrap();
+
+        if line.trim().eq("1") {
+            println!("You have {} coins.", coins);
+
+            coins -= 1;
+
+            let spin = slot_machine_utils::spin();
+            println!("{}", spin);
+
+            match spin {
+                Win => coins += 3,
+                Lose => coins += 0
+            };
+        } else if line.trim().eq("0") {
+            println!("Good-bye!");
+            break;
+        } else {
+            println!("Invalid Input.")
+        }
+    }
+}
+
 fn main() {
-    
+    greet();
+    let mut coins: usize = 10;
+    play(coins);
 }
